8e608354ae7199d46fa81bc4204ae27390f8f4df
HerbertKayky <herbertkayky9@gmail.com>
2024-04-13 17:35:28 -0300

    feat: api criada e consumida

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
 import { BsCartPlus } from "react-icons/bs";
+import { api } from "../../services/api";
+
+interface ProductProps {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  cover: string;
+}
 
 const Home = () => {
+  const [products, setProducts] = useState<ProductProps[]>([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      const response = await api.get("/products");
+      setProducts(response.data);
+    }
+
+    getProducts();
+  }, []);
+
   return (
     <div>
       <main className="w-full max-w-7xl px-4 mx-auto">
@@ -8,23 +29,32 @@ const Home = () => {
           Produtos em alta
         </h1>
 
-        <div className="grid grid-cols-1 gap-6 md:grid-cols-3 lg:grid-cols-5">
-          <section className="w-full">
-            <img
-              className="2-full rounded-lg max-h-70 mb-2"
-              src="https://i.zst.com.br/thumbs/12/b/18/1457943551.jpg"
-              alt="Logo do produto"
-            />
-            <p className="font-medium mt-1 mb-2 text-center">
-              Manifesto do partido Comunista
-            </p>
-            <div className="flex gap-3 items-center justify-center">
-              <strong className="text-zinc-700/90">R$18,98</strong>
-              <button className="bg-zinc-900 p-1 rounded">
-                <BsCartPlus size={20} color="#fff" />
-              </button>
-            </div>
-          </section>
+        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
+          {products.map((product) => (
+            <section key={product.id} className="w-full">
+              <img
+                className="2-full rounded-lg max-h-70 mb-2"
+                src={product.cover}
+                alt={product.title}
+              />
+
+              <p className="font-medium mt-1 mb-2 text-center">
+                {product.title}
+              </p>
+
+              <div className="flex gap-3 items-center justify-center">
+                <strong className="text-zinc-700/90">
+                  {product.price.toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  })}
+                </strong>
+                <button className="bg-zinc-900 p-1 rounded">
+                  <BsCartPlus size={20} color="#fff" />
+                </button>
+              </div>
+            </section>
+          ))}
         </div>
       </main>
     </div>
