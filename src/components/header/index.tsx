863b8941dbe1b08b081bd8a70c4ef71c46cc8326
HerbertKayky <herbertkayky9@gmail.com>
2024-04-13 18:07:03 -0300

    feat: configurando context api

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,11 @@
+import { useContext } from "react";
 import { FiShoppingCart } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import { CartContext } from "../../context/CartContext";
 
 const Header = () => {
+  const { cartAmount } = useContext(CartContext);
+
   return (
     <header className="w-full px-1 bg-slate-200">
       <nav className="flex w-full max-w-7xl h-14 items-center justify-between px-5 mx-auto">
@@ -11,9 +15,11 @@ const Header = () => {
 
         <Link className="relative" to="/cart">
           <FiShoppingCart size={24} color="#121212" />
-          <span className="absolute -top-3 -right-3 px-2.5 bg-sky-500 rounded-full w-5 h-5 flex items-center justify-center text-white text-xs">
-            2
-          </span>
+          {cartAmount > 0 && (
+            <span className="absolute -top-3 -right-3 px-2.5 bg-sky-500 rounded-full w-5 h-5 flex items-center justify-center text-white text-xs">
+              {cartAmount}
+            </span>
+          )}
         </Link>
       </nav>
     </header>
