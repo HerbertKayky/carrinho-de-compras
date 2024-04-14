d123aae1859012727774e990d4943ba34c61641a
HerbertKayky <herbertkayky9@gmail.com>
2024-04-14 16:45:50 -0300

    feat: listando carrinho de compras

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,30 +1,80 @@
+import { useEffect, useState, useContext } from "react";
+import { api } from "../../services/api";
+import { CartContext } from "../../context/CartContext";
+import { Link } from "react-router-dom";
+
+export interface ProductProps {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  cover: string;
+}
+
 const Cart = () => {
+  const { cart } = useContext(CartContext);
+  const [products, setProducts] = useState<ProductProps[]>([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      const response = await api.get("/products");
+      setProducts(response.data);
+    }
+
+    getProducts();
+  }, []);
+
   return (
     <div className="w-full max-w-7xl mx-auto">
       <h1 className="font-medium text-2xl text-center my-4">Meu carrinho</h1>
 
-      <section className="flex items-center justify-between border-b-2 border-gray-300">
-        <img
-          className="w-28"
-          src="https://i.zst.com.br/thumbs/12/b/18/1457943551.jpg"
-          alt="Logo do produto"
-        />
-
-        <strong>Preço: R$19,98</strong>
-        <div className="flex items-center justify-center gap-3">
-          <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
-            -
-          </button>
-          1
-          <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
-            +
-          </button>
+      {cart.length === 0 && (
+        <div className="flex flex-col items-center justify-center">
+          <p className="font-medium">Ops seu carrinho está vazio...</p>
+          <Link
+            className="bg-slate-600 my-3 p-1 px-3 text-white font-medium rounded"
+            to="/"
+          >
+            Acessar produtos
+          </Link>
         </div>
+      )}
 
-        <strong className="float-right">SubTotal: R$19,98</strong>
-      </section>
+      {cart.map((item) => (
+        <section
+          key={item.id}
+          className="flex items-center justify-between border-b-2 border-gray-300"
+        >
+          <img className="w-28" src={item.cover} alt={item.title} />
 
-      <p className="font-bold mt-4">Total: R$19,98</p>
+          <strong>
+            Preço:{" "}
+            {item.price.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })}
+          </strong>
+          <div className="flex items-center justify-center gap-3">
+            <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+              -
+            </button>
+            {item.amount}
+            <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+              +
+            </button>
+          </div>
+
+          <strong className="float-right">
+            SubTotal:{" "}
+            {item.total.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })}
+          </strong>
+        </section>
+      ))}
+
+      {cart.length !== 0 && <p className="font-bold mt-4">Total: {}</p>}
     </div>
   );
 };
